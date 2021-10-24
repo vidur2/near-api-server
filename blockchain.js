40bff62a47ee02be9bce35f656995f2662d5e193
vidur2 <55110959+vidur2@users.noreply.github.com>
2021-10-23 23:02:38 -0400

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -79,7 +79,7 @@ module.exports = {
 
     DeployContract: async function (account_id, private_key, contract_file) {
         try {
-            const path = `/home/pi/near-rest-api-server/contracts/${contract_file}`;
+            const path = `/home/pi/near-rest-api-server/contracts/voting_smart_contract_indiv.wasm`;
             if (!fs.existsSync(path))
                 return api.reject("Contract not found");
 
